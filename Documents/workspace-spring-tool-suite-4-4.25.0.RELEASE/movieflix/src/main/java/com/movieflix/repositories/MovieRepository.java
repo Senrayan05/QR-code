ac83151088e4eed88a4c6266e37367971dcef4a8
A Senrayan <143725239+Senrayan05@users.noreply.github.com>
2024-09-28 18:02:21 +0530

    Delete Documents/workspace-spring-tool-suite-4-4.25.0.RELEASE/movieflix directory

--- Documents/workspace-spring-tool-suite-4-4.25.0.RELEASE/movieflix/src/main/java/com/movieflix/repositories/MovieRepository.java
+++
@@ -1,9 +0,0 @@
-package com.movieflix.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.movieflix.entities.Movies;
-
-public interface MovieRepository extends JpaRepository<Movies, Long>{
-
-}
